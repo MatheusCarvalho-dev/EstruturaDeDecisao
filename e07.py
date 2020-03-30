5d1ff30e8c946da5c6161579cd03ec601540ba52
Matheus <60353870+McarvalhoM@users.noreply.github.com>
2020-03-30 20:18:05 -0300

    Create e07.py

---
+++ e07.py
@@ -0,0 +1,18 @@
+# Faça um Programa que leia três números e mostre o maior e o menor deles.
+
+num1 = int(input('Digite um número'))
+num2 = int(input('Digite um número'))
+num3 = int(input('Digite um número'))
+
+if num1 > num2 and num1 > num3:
+    print(num1)
+elif num1 < num2 and num2 > num3:
+    print(num2)
+elif num1 < num2 and num2 < num3:
+    print(num3)
+if num1 < num2 and num1 < num3:
+    print(num1)
+elif num2 < num1 and num2 < num3:
+    print(num2)
+elif num3 < num1 and num3 < num2:
+    print(num3)
