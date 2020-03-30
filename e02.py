977fe64f09eeea347f224f6cde18decb603b4ba7
Matheus <60353870+McarvalhoM@users.noreply.github.com>
2020-03-30 14:42:43 -0300

    Update e02.py

--- e02.py
+++ e02.py
@@ -1,1 +1,9 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
 
+num1 = float(input('Digite um número'))
+if num1 == 0:
+    print(num1, ' é neutro (0)')
+if num1 > 0:
+    print(num1, ' é positivo!')
+if num1 < 0:
+    print(num1, ' é negativo!')
