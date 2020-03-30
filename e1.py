55dc7b109817733a75065ff64b698e179a353a7f
Matheus <60353870+McarvalhoM@users.noreply.github.com>
2020-03-30 14:26:37 -0300

    Update e1.py

--- e1.py
+++ e1.py
@@ -1,1 +1,10 @@
+#Faça um Programa que peça dois números e imprima o maior deles. 
 
+num1= int(input('Digite o primeiro número'))
+num2= int(input('Digite o segundo número'))
+
+if num1 > num2
+    print('O maior número é: ',num1)
+else:
+    print('O maior número é: ',num2)
+    
