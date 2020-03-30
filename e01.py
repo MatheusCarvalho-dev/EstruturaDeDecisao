6b4161f319ba8197e6d293641fa8dd2b6b19b259
Matheus <60353870+McarvalhoM@users.noreply.github.com>
2020-03-30 14:29:58 -0300

    Update e01.py

--- e01.py
+++ e01.py
@@ -3,7 +3,7 @@
 num1= int(input('Digite o primeiro número'))
 num2= int(input('Digite o segundo número'))
 
-if num1 > num2
+if num1 > num2:
     print('O maior número é: ',num1)
 else:
     print('O maior número é: ',num2)
